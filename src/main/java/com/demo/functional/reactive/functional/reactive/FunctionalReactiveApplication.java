f91b09ec34cf147671de3545691fdccc9b176aa0
adwaita <codeespice@gmail.com>
2023-07-20 11:45:42 -0500

    init commit

---
+++ src/main/java/com/demo/functional/reactive/functional/reactive/FunctionalReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.demo.functional.reactive.functional.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalReactiveApplication.class, args);
+	}
+
+}
