26d4897352b26889897222443d38e85fa43fa975
codeespice <codespice@gmail.com>
2023-07-20 12:59:25 -0500

    convert to maven

---
+++ src/main/java/com/example/functional/reactive/FunctionalReactiveApplication.java
@@ -0,0 +1,10 @@
+package com.example.functional.reactive;
+
+
+public class FunctionalReactiveApplication {
+
+	public static void main(String[] args) {
+		System.out.println("oam vigneswara");
+	}
+
+}
